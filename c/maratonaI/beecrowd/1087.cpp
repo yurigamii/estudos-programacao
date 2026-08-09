96e8402b1ae2466f40b34d85abf41d58b9eea8b9
yurigamii <yuriferxavier@gmail.com>
2026-08-09 18:05:43 -0300

    novo exercicio

--- c/maratonaI/beecrowd/1087.cpp
+++ c/maratonaI/beecrowd/1087.cpp
@@ -2,10 +2,7 @@
 
 int main(){
 
-	int x1;
-	int y1;
-	int x2;
-	int y2;
+	int x1 = 0;int y1 = 0;int x2 = 0;int y2 = 0;
 
 	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
@@ -13,18 +10,10 @@ int main(){
 		printf("0");
 	}
 
-	else if((x1 == x2) && (y1 != y2)){
+	else if((((x1 == x2) && (y1 != y2)) && ((x1 != x2) && (y1 == y2))) || (abs(x2 - x1) == abs(y2 - y1))){
 		printf("1");	
 	}
 	
-	else if((x1 != x2) && (y1 == y2)){
-		printf("1");
-	}
-	
-	else if((abs(x2) == abs(y2))){
-		printf("1");
-	}
-
 	else{
 		printf("2");
 	}
