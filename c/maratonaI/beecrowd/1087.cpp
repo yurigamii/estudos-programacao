b48d4b6b58e10b08d7dadb313d134f8e7ef654df
yurigamii <yuriferxavier@gmail.com>
2026-08-09 18:33:07 -0300

    correcao do 1087

--- c/maratonaI/beecrowd/1087.cpp
+++ c/maratonaI/beecrowd/1087.cpp
@@ -6,6 +6,25 @@ int main(){
 
 	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
+	while ((x1 && x2 && y1 && y2) != 0) {
+	
+		if((x1 == x2) && (y1 == y2)){
+			printf("0\n");
+		}
+
+		else if((((x1 == x2) && (y1 != y2)) || ((x1 != x2) && (y1 == y2))) || (abs(x2 - x1) == abs(y2 - y1))){
+			printf("1\n");	
+		}
+	
+		else{
+			printf("2\n");
+		}
+
+		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+	}
+
+/*
 	if((x1 == x2) && (y1 == y2)){
 		printf("0\n");
 	}
@@ -17,6 +36,6 @@ int main(){
 	else{
 		printf("2\n");
 	}
-
+*/
 	return 0;
 }
