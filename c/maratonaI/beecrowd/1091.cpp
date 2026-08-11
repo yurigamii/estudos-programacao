bfcfce2461c2f506ef6574344b753f773fa8af59
yurigamii <yuriferxavier@gmail.com>
2026-08-10 21:53:15 -0300

    correcao

--- c/maratonaI/beecrowd/1091.cpp
+++ c/maratonaI/beecrowd/1091.cpp
@@ -5,25 +5,33 @@ int main(){
 	int k = 0; int n = 0; int m = 0; int x = 0; int y = 0;
 
 	scanf("%d", &k);
+	while (k != 0) {
 
-	while(k != 0){
+		scanf("%d %d", &n, &m);
 
-		scanf();
-		if(((n == x) && (m == y)) || ((x == n) && (y != m)) || ((x != n) && (y == m))){
-			printf("divisa\n");	
+		for(int i = 0; i < k; i++){
+
+			scanf("%d %d", &x, &y);
+
+			if(((n == x) && (m == y)) || ((x == n) && (y != m)) || ((x != n) && (y == m))){
+				printf("divisa\n");	
+			}
+			else if ((x > n) && (y > m)) {
+				printf("NE\n");
+			}
+			else if ((x < n) && (y > m)) {
+				printf("NO\n");
+			}
+			else if((x > n) && (y < m)){
+				printf("SE\n");
+			}
+			else if((x < n) && (y < m)){
+				printf("SO\n");
+			}
+
 		}
-		else if ((x > n) && (y > m)) {
-			printf("NE\n");
-		}
-		else if ((x < n) && (y > m)) {
-			printf("NO\n");
-		}
-		else if((x > n) && (y < m)){
-			printf("SE\n");
-		}
-		else if((x < n) && (y < m)){
-			printf("SO\n");
-		}
+
+		scanf("%d",&k);
 
 	}
 
