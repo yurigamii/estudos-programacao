08ae6c285ce074a27c8d08ca437e12583f417026
yurigamii <yuriferxavier@gmail.com>
2026-08-25 14:14:54 -0300

    terminei o 1024

--- c/maratonaI/beecrowd/1024.cpp
+++ c/maratonaI/beecrowd/1024.cpp
@@ -10,17 +10,48 @@ int main(){
 	char passada2[M];
 	char passada3[M];
 
-	//scanf("%d", &n);
-	memset(passada1, 0, sizeof(passada1));
-	fgets(senha, M, stdin);
-	unsigned long tamanho = strlen(senha);
+	scanf("%d", &n);
+	getchar();
 
+	for(int j = 0; j < n; j++){
+		memset(senha, 0, sizeof(senha));
+		memset(passada1, 0, sizeof(passada1));
+		memset(passada2, 0, sizeof(passada2));
+		memset(passada3, 0, sizeof(passada3));
+
+		scanf("%999[^\n]", senha);
+		getchar();
+		unsigned long tamanho = strlen(senha);
+
+		for(int i = 0; i < tamanho; i++){
+			if(((senha[i] >= 65) && (senha[i] <= 90)) || ((senha[i] >= 97) && (senha[i] <= 122))){
+				passada1[i] = senha[i] + 3;
+
+			}
+			else{
+				passada1[i] = senha[i];
+			}
+		}
+
+		for(int i = 0; i < tamanho; i++){
+			passada2[i] = passada1[(tamanho - 1) - i];
+		}
 	
-	for(int i = 0; i < tamanho; i++){
-		passada1[i] = senha[i] + 3;
+		int metade = tamanho/2;
+
+		for(int i = 0; i < tamanho; i++){
+			if(i >= metade){
+				passada3[i] = passada2[i] - 1;
+
+			}
+			else{
+				passada3[i] = passada2[i];
+			}
+		}
+	//printf("%s\n", passada1);
+	//printf("%s\n", passada2);
+		printf("%s\n", passada3);
+	//printf("%d\n", metade);
 	}
-
-	printf("%s", passada1);
-
-	return 0;
+		return 0;
 }
